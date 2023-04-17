c11fa5b423fafdd32a123d390344e6bff0f1bbfd
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2023-04-17 12:31:42 +0100

    Help

--- java/src/ie/tudublin/Bullet.java
+++ java/src/ie/tudublin/Bullet.java
@@ -13,15 +13,13 @@ public class Bullet {
 
     int timeToLive = 5000;
     int creationTime = 0; 
+    
 
     public Bullet(float x, float y, float rot, int c, PApplet p)
     {
-        pos = new PVector(x, y);
-        forward = new PVector(0, -1);
+        super(x, y, rot, c, p);
+        
         speed = 5;
-        this.p = p;
-        this.rot = rot;
-        this.c = c;
         creationTime = p.millis();
 
     }
